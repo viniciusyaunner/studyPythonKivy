8f4f0eb57c0d5b989cec3a48cd1d883b5a5b6f75
ViniciusFigueiredo <ViniciusFigueiredo@github.com>
2018-07-07 20:54:12 -0300

    update

---
+++ source/kvlang/(01)hello_world/main.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from kivy.app import App
+
+
+class HelloApp(App):
+    pass
+
+
+HelloApp().run()
---
+++ source/kvlang/(02)medidas/main.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+
+class RootWidget(FloatLayout):
+    pass
+
+class MedidaApp(App):
+    def build(self):
+        return RootWidget()
+MedidaApp().run()
